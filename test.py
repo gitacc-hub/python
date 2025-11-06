08fe830d4462e809281eedf2ce83ec0b9749824c
gitacc-hub <github2001acc@gmail.com>
2025-11-06 21:26:25 +0300

    Remove unnecessary text file

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 #Welcome to a RegEx tutorial
 #I am testing RegEx findall function here
+"""
 import re
 numbers="My phone numbers 078922938221 and 0748889448"
-pattern=r"\d+"
+pattern=r\d+
 search=re.findall(pattern,numbers)
-print(search)
+print(search)"""
+
+import os
+os.remove("line.txt")
